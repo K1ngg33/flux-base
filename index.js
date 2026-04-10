b3b00819accf29739d0f6cb9ea93c90b9aa44ac4
K1ngg33 <alanalmeidaneves8@gmail.com>
2026-04-10 09:18:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Inicio de index.js.
+const { ForgeClient } = require("@tryforge/forgescript");
+const { ForgeDB } = require("@tryforge/forge.db");
+
+const client = new ForgeClient({
+    intents: [
+        "Guilds",
+        "GuildMembers",
+        "GuildModeration",
+        "GuildEmojisAndStickers",
+        "GuildIntegrations",
+        "GuildWebhooks",
+        "GuildInvites",
+        "GuildVoiceStates",
+        "GuildPresences",
+        "GuildMessages",
+        "GuildMessageReactions",
+        "GuildMessageTyping",
+        "DirectMessages",
+        "DirectMessageReactions",
+        "DirectMessageTyping",
+        "MessageContent",
+        "GuildScheduledEvents",
+        "AutoModerationConfiguration",
+        "AutoModerationExecution",
+        "GuildMessagePolls",
+        "DirectMessagePolls"
+    ],
+    extensions: [
+    new ForgeDB()
+    ],
+    prefixes: [
+        ".",
+        "..",
+        "?",
+        "!"
+    ]
+});
+console.log("Tudo funcionando");
+client.functions.load('./Funções');
+client.commands.load('./Handler/cmds');
+client.applicationCommands.load('./Handler/slsh');
+
+client.login("SEU-TOKEN (recomendamos colocar em um .env)");
